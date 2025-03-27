35c1aa0d30f95bce8ff97db087d57d0a0d235f2f
Brian <briansubekti11@gmail.com>
2025-03-28 00:05:11 +0700

    Implement list_all_as_string function in Subscriber repository.

--- src/repository/notification.rs
+++ src/repository/notification.rs
@@ -17,4 +17,9 @@ use lazy_static::lazy_static;
              .push(notification.clone());
          return notification;
      }
+
+     pub fn list_all_as_string() -> Vec<String> {
+        return NOTIFICATIONS.read().unwrap()
+            .iter().map(|f| format!("{}", f.clone())).collect();
+    }
  }
