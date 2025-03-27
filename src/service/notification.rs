07c94378bdd2cfb0b611edc4d04ab0e0926a1592
Brian <briansubekti11@gmail.com>
2025-03-28 00:18:02 +0700

    Implement receive_notification function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -90,4 +90,8 @@ use rocket::http::Status;
             ))
         }
     }
+    pub fn receive_notification(payload: Notification) -> Result<Notification> {
+        let subscriber_request: Notification = NotificationRepository::add(payload);
+        return Ok(subscriber_request);
+    }
  }
